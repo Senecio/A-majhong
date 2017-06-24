d672f6685505fae857346b1868d5c851ddf9a727
lishihai <jpweiyi@163.com>
2017-06-24 14:32:34 +0800

    6

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ var app = express();
 
 // 设置客户端根目录
 app.use(express.static(__dirname + "/public"));
+
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 // 监听端口
 var listenPort = process.env.LEANCLOUD_APP_PORT || 80;
 app.listen( listenPort, function() {
